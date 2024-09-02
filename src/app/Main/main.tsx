1e667557a48d652e900cf416077844fef4d54a52
Augusto Meireles <augusto.meireles@bhave.life>
2024-09-02 16:09:29 -0300

    feat: hello world

---
+++ src/app/Main/main.tsx
@@ -0,0 +1,10 @@
+const Main = () => {
+    return (
+        <main>
+            <h1 className="text-pink-300">Hello World!</h1>
+            <p className="text-pink-500">Apresentando Landing Page do Tavius Tipster</p>
+        </main>
+    )
+}
+
+export default Main;
