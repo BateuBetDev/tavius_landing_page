67813e2f7419f46a74def6836b8f07d0b144f1c0
Augusto Meireles <augusto.meireles@bhave.life>
2024-09-05 16:30:05 -0300

    main

--- src/app/Main/main.tsx
+++ src/app/Main/main.tsx
@@ -13,13 +13,14 @@ const Main = () => {
                 </a>
             </div>
             {/* Conteúdo Imagem + Texto */}
-            <div className='px-4 py-2 w-full h-[80%] bg-main bg-cover bg-no-repeat bg-mobile-position md:bg-tablet-position xl:bg-contain xl:bg-desktop-position'>
+            <div className='px-4 py-2 w-full h-[80%] bg-main bg-cover bg-no-repeat bg-mobile-position md:bg-tablet-position xl:bg-contain xl:bg-desktop-position md:px-8 xl:px-10'>
                 <div className='w-full h-full flex items-end md:w-1/2 md:items-center'>
-                    <h2 className='text-white font-rowdies text-xl uppercase md:text-3xl'>
+                    <h2 className='text-white font-rowdies text-xl uppercase md:text-3xl xl:text-[2.5rem]'>
                         Eu vou te <span className='text-yellow-400'>ensinar </span> as estratégias que me renderam <span className='text-yellow-400'>muita grana</span>. Todos os dias e ao vivo, você vai ter acesso a todo o meu arsenal que me fizeram <span className='text-yellow-400'>mudar de vida</span>.
                     </h2>
                 </div>
             </div>
+            {/* Botão */}
             <div className='pt-4 w-full flex justify-center'>
                 <Button/>
             </div>
